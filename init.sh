a7781fc5b789a1ce13c7704fbf4e076b2c08f432
Jeonneung OH <aroma0560@gmail.com>
2025-06-24 07:17:41 +0000

    Settings

---
+++ init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# 기본 툴 설치
+sudo apt-get update
+sudo apt-get install -y net-tools iputils-ping unzip curl gnupg lsb-release
+pip install httpie
+
+# kubectl 설치
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Node.js 설치 (필요 시)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 14.19.0 && nvm use 14.19.0
+export NODE_OPTIONS=--openssl-legacy-provider
+
+# Azure CLI 설치
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+rm microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/azure-cli.list'
+
+sudo apt update
+sudo apt install azure-cli -y
+
+# 인프라 실행 (필요 시)
+cd infra
+docker-compose up
